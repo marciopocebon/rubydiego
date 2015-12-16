47c4c1c7a34ae562a74ed1d0052c2d8a6fa30d46
Diego Fernandes <diego.schell.f@gmail.com>
2015-12-16 23:09:46 +0000

    remove slow video properties

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,9 @@ Rails.application.configure do
 
   config.active_record.migration_error = :page_load
 
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.assets.digest = true
 
-  config.assets.raise_runtime_errors = true
+  config.assets.raise_runtime_errors = false
 end
